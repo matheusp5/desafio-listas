3e9e99858a27c826eae076ff9fa9cd510e9f97b6
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-09-22 17:29:24 -0300

    feat(screens): initial screen implementation

---
+++ src/br/com/mxtheuz/screens/Screens.java
@@ -0,0 +1,12 @@
+package br.com.mxtheuz.screens;
+
+public class Screens {
+
+    private final double CardLimit = 0;
+
+    public void InitScreen() {
+        System.out.println();
+        System.out.println("Olá! Seja bem-vindo.");
+        System.out.println();
+    }
+}
