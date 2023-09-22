538564ac2323d8d949def82971c919ebedfebf32
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-09-22 17:31:09 -0300

    feat(screens): scanner, name

--- src/br/com/mxtheuz/screens/Screens.java
+++ src/br/com/mxtheuz/screens/Screens.java
@@ -1,12 +1,20 @@
 package br.com.mxtheuz.screens;
 
+import java.util.Scanner;
+
 public class Screens {
 
-    private final double CardLimit = 0;
+    private double CardLimit = 0;
+    private String name = "";
+    private final Scanner scanner = new Scanner(System.in);
+
 
     public void InitScreen() {
         System.out.println();
         System.out.println("Olá! Seja bem-vindo.");
+        System.out.print("Para começar, iniciar o seu nome: ");
+        this.name = scanner.next();
+
         System.out.println();
     }
 }
